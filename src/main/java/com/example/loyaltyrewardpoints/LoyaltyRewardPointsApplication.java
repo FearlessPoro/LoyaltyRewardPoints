2edbb31af03a2d0fe3b81118ae5581a1ae85349a
FearlessPoro <wojtek.franczuk@gmail.com>
2023-12-10 00:33:22 +0100

    Initial commit

---
+++ src/main/java/com/example/loyaltyrewardpoints/LoyaltyRewardPointsApplication.java
@@ -0,0 +1,13 @@
+package com.example.loyaltyrewardpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyRewardPointsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoyaltyRewardPointsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loyaltyrewardpoints/LoyaltyRewardPointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loyaltyrewardpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyRewardPointsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
