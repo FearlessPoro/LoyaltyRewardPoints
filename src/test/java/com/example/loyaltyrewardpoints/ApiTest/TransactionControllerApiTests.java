43d0bcb317419b8644e2beebb139358ce9e76eb0
FearlessPoro <wojtek.franczuk@gmail.com>
2023-12-11 12:51:18 +0100

    Add more API tests

--- src/test/java/com/example/loyaltyrewardpoints/ApiTest/TransactionControllerApiTests.java
+++ src/test/java/com/example/loyaltyrewardpoints/ApiTest/TransactionControllerApiTests.java
@@ -2,15 +2,23 @@ package com.example.loyaltyrewardpoints.ApiTest;
 
 import com.example.loyaltyrewardpoints.dto.ApplicationUserDto;
 import com.example.loyaltyrewardpoints.dto.TransactionDto;
+import com.example.loyaltyrewardpoints.model.Transaction;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.*;
+import org.springframework.http.HttpStatus;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TransactionControllerApiTests {
@@ -32,7 +40,7 @@ public class TransactionControllerApiTests {
                 .body(userDto)
                 .post("/user")
                 .then()
-                .statusCode(201);
+                .statusCode(HttpStatus.CREATED.value());
 
         TransactionDto transactionDto = new TransactionDto();
         transactionDto.setAmount(BigDecimal.valueOf(10000.0));
@@ -44,7 +52,7 @@ public class TransactionControllerApiTests {
                 .when()
                 .post("/transaction/{userId}")
                 .then()
-                .statusCode(201)
+                .statusCode(HttpStatus.CREATED.value())
                 .body("amount", equalTo(10000.0F));
     }
 
@@ -58,7 +66,7 @@ public class TransactionControllerApiTests {
                 .when()
                 .post("/transaction/{userId}")
                 .then()
-                .statusCode(400)
+                .statusCode(HttpStatus.BAD_REQUEST.value())
                 .body("error", equalTo("Bad Request"))
                 .body("message", containsString("User with that ID does not exist"));
     }
@@ -73,7 +81,7 @@ public class TransactionControllerApiTests {
                 .when()
                 .put("/transaction/{transactionId}")
                 .then()
-                .statusCode(200)
+                .statusCode(HttpStatus.OK.value())
                 .body("amount", equalTo(20000.0F));
     }
 
@@ -87,10 +95,39 @@ public class TransactionControllerApiTests {
                 .when()
                 .put("/transaction/{transactionId}")
                 .then()
-                .statusCode(400)
+                .statusCode(HttpStatus.BAD_REQUEST.value())
                 .body("error", equalTo("Bad Request"))
                 .body("message", containsString("Transaction with that ID does not exist"));
     }
 
+    @Test
+    @Order(5)
+    public void testGetAllUserTransactionsFromTimePeriod_Success() {
+        final Long applicationUserId = 1L;
+        final DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+
+        final LocalDateTime startTime = LocalDateTime.now().minusDays(1);
+        final LocalDateTime endTime = LocalDateTime.now();
+
+        // Format the date using the specified pattern
+        final String formattedStartTime = startTime.format(formatter);
+        final String formattedEndTime = endTime.format(formatter);
+
+        final Response response = given()
+                .param("startTime", formattedStartTime)
+                .param("endTime", formattedEndTime)
+                .when()
+                .get("/transaction/" + applicationUserId + "/time-period")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .contentType(ContentType.JSON)
+                .extract()
+                .response();
+
+        List<Transaction> userTransactions = Arrays.asList(response.getBody().as(Transaction[].class));
+
+        assertEquals(1, userTransactions.size());
+        assertEquals(BigDecimal.valueOf(20000.00).setScale(2), userTransactions.get(0).getAmount());
+    }
 
 }
